f1e1e0e885fbea6b108add360c18e236ac6eb813
Mateusz Oleksik <UL0240010@edu.uni.lodz.pl>
2022-03-24 17:43:46 +0100

    MainActivity cleanup

--- app/src/main/java/mateusz/oleksik/remindeme/MainActivity.kt
+++ app/src/main/java/mateusz/oleksik/remindeme/MainActivity.kt
@@ -3,16 +3,10 @@ package mateusz.oleksik.remindeme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import androidx.camera.core.ImageCapture
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import mateusz.oleksik.remindeme.fragments.FoodCreateFragment
 import mateusz.oleksik.remindeme.fragments.FoodListFragment
 import mateusz.oleksik.remindeme.interfaces.FoodCreateDialogListener
-import java.time.LocalDate
-import java.util.*
 
 class MainActivity() : AppCompatActivity(), FoodCreateDialogListener {
 
@@ -24,7 +18,7 @@ class MainActivity() : AppCompatActivity(), FoodCreateDialogListener {
 
         if (savedInstanceState == null) {
 
-            var fragmentTag: String = "foodList"
+            val fragmentTag = "foodList"
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.root_layout, FoodListFragment.newInstance(), fragmentTag)
@@ -33,21 +27,16 @@ class MainActivity() : AppCompatActivity(), FoodCreateDialogListener {
 
             foodListFragment = supportFragmentManager.findFragmentByTag(fragmentTag) as FoodListFragment
 
-            if (foodListFragment != null){
-
-                val addButton: FloatingActionButton = findViewById(R.id.add_food_action_button)
-
-                addButton.setOnClickListener{
-                    var dialog = FoodCreateFragment(this)
-                    dialog.show(supportFragmentManager, "createFoodDialog")
-                    //foodListFragment.addFoodToAdapter(Food(0, "Agrest", 20220101))
-                }
+            val addButton: FloatingActionButton = findViewById(R.id.add_food_action_button)
+            addButton.setOnClickListener{
+                val dialog = FoodCreateFragment(this)
+                dialog.show(supportFragmentManager, "createFoodDialog")
             }
         }
     }
 
     override fun onCreatedFood(food: Food) {
-        Log.i("APPINFO", "On created food called")
+        Log.i("INFO", "On created food called")
         foodListFragment.addFoodToAdapter(food)
     }
 }
