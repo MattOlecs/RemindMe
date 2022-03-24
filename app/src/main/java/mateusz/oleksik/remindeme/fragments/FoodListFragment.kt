737da91d057ab95f3a171b4dc9a6551afdfe7445
Mateusz Oleksik <UL0240010@edu.uni.lodz.pl>
2022-03-24 20:16:56 +0100

    FoodListFragment data binding

--- app/src/main/java/mateusz/oleksik/remindeme/fragments/FoodListFragment.kt
+++ app/src/main/java/mateusz/oleksik/remindeme/fragments/FoodListFragment.kt
@@ -6,16 +6,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.runBlocking
 import mateusz.oleksik.remindeme.Food
 import mateusz.oleksik.remindeme.adapters.FoodAdapter
-import mateusz.oleksik.remindeme.R
 import mateusz.oleksik.remindeme.database.repositories.FoodStorageRepository
+import mateusz.oleksik.remindeme.databinding.FragmentFoodListBinding
 import mateusz.oleksik.remindeme.interfaces.FoodItemClickListener
+import mateusz.oleksik.remindeme.utils.Constants
 
 class FoodListFragment : Fragment(), FoodItemClickListener {
 
@@ -33,68 +32,62 @@ class FoodListFragment : Fragment(), FoodItemClickListener {
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
-        if (context != null){
-
-            Log.i("APPINFO", "Initializing food storage repository in FoodListFragment")
-            initRepo(context)
-            Log.i("APPINFO", "Initializing food list in FoodListFragment")
-            initList()
-            Log.i("APPINFO", "Initializing adapter in FoodListFragment")
-            initAdapter()
-        }
+        Log.i(Constants.DebugLogTag, "Initializing food storage repository in FoodListFragment")
+        initRepo(context)
+        Log.i(Constants.DebugLogTag, "Initializing food list in FoodListFragment")
+        initList()
+        Log.i(Constants.DebugLogTag, "Initializing adapter in FoodListFragment")
+        initAdapter()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        val view: View = inflater.inflate(R.layout.fragment_food_list, container, false)
+    ): View {
+        val fragmentFoodListBinding =
+            FragmentFoodListBinding.inflate(inflater, container, false)
         val activity = activity as Context
-        val recyclerView = view.findViewById<RecyclerView>(R.id.food_list_recycler_view)
+        val recyclerView = fragmentFoodListBinding.foodListRecyclerView
 
         recyclerView.layoutManager = GridLayoutManager(activity, 1)
         recyclerView.adapter = foodAdapter
 
-        return view
+        return fragmentFoodListBinding.root
     }
 
-    private fun initRepo(context: Context){
+    private fun initRepo(context: Context) {
         runBlocking {
             foodStorageRepository = FoodStorageRepository(context)
         }
     }
 
-    private fun initList(){
+    private fun initList() {
         runBlocking {
             foodList = foodStorageRepository.getAll().toMutableList()
         }
     }
 
-    private fun initAdapter(){
+    private fun initAdapter() {
         foodAdapter = FoodAdapter(foodList, this)
     }
 
-
-    fun addFoodToAdapter(food: Food){
+    fun addFoodToAdapter(food: Food) {
         runBlocking { foodStorageRepository.insertAll(food) }
-        foodAdapter.addFood(food);
+        foodAdapter.addFood(food)
 
-        Toast.makeText(
-            context,
-            "Succesfully added new food: ${food.name}",
-            Toast.LENGTH_SHORT)
-            .show()
+        Log.i(
+            Constants.InfoLogTag,
+            "Successfully added new food: ${food.name}"
+        )
     }
 
     override fun foodItemClicked(position: Int, foodToDelete: Food) {
         runBlocking { foodStorageRepository.delete(foodToDelete) }
-        Toast.makeText(
-            context,
-            "Deletion succesful at postition: ${position}. Name of deleted object: ${foodToDelete.name}",
-            Toast.LENGTH_SHORT)
-            .show()
-    }
 
+        Log.i(
+            Constants.InfoLogTag,
+            "Deletion successful at position: ${position}. Name of deleted object: ${foodToDelete.name}"
+        )
+    }
 }
