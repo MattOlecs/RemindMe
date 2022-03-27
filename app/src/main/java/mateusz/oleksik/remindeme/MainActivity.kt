297adc98874d27bffd2c6b0000a9e0e96787c751
Mateusz Oleksik <UL0240010@edu.uni.lodz.pl>
2022-03-27 21:29:51 +0200

    Main activity data binding

--- app/src/main/java/mateusz/oleksik/remindeme/MainActivity.kt
+++ app/src/main/java/mateusz/oleksik/remindeme/MainActivity.kt
@@ -3,6 +3,8 @@ package mateusz.oleksik.remindeme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.dynamic.SupportFragmentWrapper
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import mateusz.oleksik.remindeme.databinding.ActivityMainBinding
 import mateusz.oleksik.remindeme.fragments.FoodCreateFragment
@@ -17,31 +19,40 @@ class MainActivity() : AppCompatActivity(), IFoodCreateDialogListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         if (savedInstanceState == null) {
+            generateFoodList()
 
-            val fragmentTag = "foodList"
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.root_layout, FoodListFragment.newInstance(), fragmentTag)
-                .commit()
-            supportFragmentManager.executePendingTransactions()
-            foodListFragment = supportFragmentManager.findFragmentByTag(fragmentTag) as FoodListFragment
-
-            binding.addFoodActionButton.setOnClickListener{
-                val dialog = FoodCreateFragment(this)
-                dialog.show(supportFragmentManager, "createFoodDialog")
+            binding.addFoodActionButton.setOnClickListener {
+                openCreateFoodDialog()
             }
         }
+    }
+
+    private fun generateFoodList() {
+        val fragmentTag = "foodList"
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.root_layout, FoodListFragment.newInstance(), fragmentTag)
+            .commit()
+        supportFragmentManager.executePendingTransactions()
+
+        foodListFragment =
+            supportFragmentManager.findFragmentByTag(fragmentTag) as FoodListFragment
+    }
+
+    private fun openCreateFoodDialog() {
+        val dialog = FoodCreateFragment(this)
+        dialog.show(supportFragmentManager, "createFoodDialog")
     }
 
     override fun onCreatedFood(food: Food) {
         Log.i(
             Constants.DebugLogTag,
-            "Main activity communicated new food creation to Adapter. Food: ${food.name}")
+            "Main activity communicated new food creation to Adapter. Food: ${food.name}"
+        )
         foodListFragment.addFoodToAdapter(food)
     }
 }
--- app/src/main/java/mateusz/oleksik/remindeme/fragments/FoodCreateFragment.kt
+++ app/src/main/java/mateusz/oleksik/remindeme/fragments/FoodCreateFragment.kt
@@ -15,104 +15,99 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import mateusz.oleksik.remindeme.Food
-import mateusz.oleksik.remindeme.R
+import mateusz.oleksik.remindeme.databinding.FragmentCreateFoodBinding
 import mateusz.oleksik.remindeme.interfaces.IFoodCreateDialogListener
 import mateusz.oleksik.remindeme.utils.CameraUtils
 
 class FoodCreateFragment(
-    private val listenerI: IFoodCreateDialogListener
-    ) : DialogFragment() {
+    private val listener: IFoodCreateDialogListener
+) : DialogFragment() {
 
-    private lateinit var imageView: ImageView
-    private lateinit var nameTextView: TextView
+    private lateinit var binding: FragmentCreateFoodBinding
     private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+    private var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
 
-    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data?.extras
+                val bitmap = data?.get("data") as Bitmap
+                val camComp = CameraUtils.getRotationCompensation(requireActivity())
+                val image = InputImage.fromBitmap(bitmap, 0)
 
-            val data = result.data?.extras
-            val bitmap = data?.get("data") as Bitmap
-            val camComp = CameraUtils.getRotationCompensation(requireActivity())
-            val image = InputImage.fromBitmap(bitmap, 0)
-            imageView.setImageBitmap(bitmap)
+                binding.imageView.setImageBitmap(bitmap)
 
-            recognizer.process(image)
-                .addOnSuccessListener { visionText ->
-                    var resultText = visionText.textBlocks[0].text
-                    nameTextView.text = resultText
-                    Toast.makeText(context,"Text recognition succesful", Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener { e ->
-                    Toast.makeText(context,"Nie udało się! Camera compensation: $camComp. ${e.localizedMessage}", Toast.LENGTH_SHORT).show()
-                }
+                recognizer.process(image)
+                    .addOnSuccessListener { visionText ->
+                        val resultText = visionText.textBlocks[0].text
+                        binding.foodCreateNameTextView.setText(resultText)
+                        Toast.makeText(context, "Text recognition successful", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    .addOnFailureListener { e ->
+                        Toast.makeText(
+                            context,
+                            "Text recognition failed: $camComp. ${e.localizedMessage}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+            }
         }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentCreateFoodBinding.inflate(inflater, container, false)
 
-        val view: View = inflater.inflate(R.layout.fragment_create_food, container, false)
-
-        val cancelButton = view.findViewById<Button>(R.id.food_create_cancel_button)
-        val createButton = view.findViewById<Button>(R.id.food_create_confirm_button)
-        nameTextView = view.findViewById<TextView>(R.id.food_create_name_text_view)
-        val calendarView = view.findViewById<CalendarView>(R.id.food_create_calendar_view)
-        val cameraButton = view.findViewById<FloatingActionButton>(R.id.food_create_camera_button)
-        imageView = view.findViewById<ImageView>(R.id.imageView)
-
-        calendarView.setOnDateChangeListener{calendarView, year, month, dayOfMonth ->
-
+        binding.foodCreateCalendarView.setOnDateChangeListener { calendarView, year, month, dayOfMonth ->
             calendarView.date = getDateFromInt(year, month, dayOfMonth)
         }
-
-        cancelButton.setOnClickListener{
+        binding.foodCreateCancelButton.setOnClickListener {
             dismiss()
         }
+        binding.foodCreateConfirmButton.setOnClickListener {
+            val foodName = binding.foodCreateNameTextView.text.toString()
+            val expirationDate = binding.foodCreateCalendarView.date
 
-        createButton.setOnClickListener{
-            val foodName = nameTextView.text.toString()
-            val expirationDate = calendarView.date
-
-
-            listenerI.onCreatedFood(Food(0, foodName, expirationDate))
+            listener.onCreatedFood(Food(0, foodName, expirationDate))
             dismiss()
         }
-
-        cameraButton.setOnClickListener{
+        binding.foodCreateCameraButton.setOnClickListener {
             getExpirationDateFromImage()
         }
 
-        return view
+        return binding.root
     }
 
-    private fun getDateFromInt(year: Int, month: Int, dayOfMonth: Int) : Long{
+    private fun getDateFromInt(year: Int, month: Int, dayOfMonth: Int): Long {
         val calendar = Calendar.getInstance()
         calendar.set(year, month, dayOfMonth)
 
         return calendar.time.time
     }
 
-    private fun getExpirationDateFromImage(){
+    private fun getExpirationDateFromImage() {
 
         if (allPermissionsGranted()) {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             resultLauncher.launch(intent)
         } else {
             ActivityCompat.requestPermissions(
-                requireActivity(), CameraUtils.CAMERA_REQUIRED_PERMISSIONS, CameraUtils.CAMERA_REQUEST_CODE_PERMISSIONS)
+                requireActivity(),
+                CameraUtils.CAMERA_REQUIRED_PERMISSIONS,
+                CameraUtils.CAMERA_REQUEST_CODE_PERMISSIONS
+            )
         }
     }
 
     private fun allPermissionsGranted() = CameraUtils.CAMERA_REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
-            requireContext(), it) == PackageManager.PERMISSION_GRANTED
+            requireContext(), it
+        ) == PackageManager.PERMISSION_GRANTED
     }
 }
