1f2bf923f8d9491857ebe450a4d722183e37636a
MattOlecs <80700054+MattOlecs@users.noreply.github.com>
2022-03-29 16:59:15 +0200

    OCR improvements and UI fixes

--- app/src/main/java/mateusz/oleksik/remindeme/fragments/FoodCreateFragment.kt
+++ app/src/main/java/mateusz/oleksik/remindeme/fragments/FoodCreateFragment.kt
@@ -15,13 +15,19 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import mateusz.oleksik.remindeme.models.Food
 import mateusz.oleksik.remindeme.databinding.FragmentCreateFoodBinding
 import mateusz.oleksik.remindeme.interfaces.IFoodCreateDialogListener
 import mateusz.oleksik.remindeme.utils.CameraUtils
+import mateusz.oleksik.remindeme.utils.DateUtils
+import mateusz.oleksik.remindeme.utils.Extensions.Companion.toShortDateString
 
 class FoodCreateFragment(
     private val listener: IFoodCreateDialogListener
@@ -29,7 +35,8 @@ class FoodCreateFragment(
 
     private lateinit var binding: FragmentCreateFoodBinding
     private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-    private var resultLauncher =
+
+    private var dateRecognitionResultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
 
@@ -38,19 +45,62 @@ class FoodCreateFragment(
                 val camComp = CameraUtils.getRotationCompensation(requireActivity())
                 val image = InputImage.fromBitmap(bitmap, 0)
 
-                binding.imageView.setImageBitmap(bitmap)
 
                 recognizer.process(image)
                     .addOnSuccessListener { visionText ->
-                        val resultText = visionText.textBlocks[0].text
-                        binding.foodCreateNameTextView.setText(resultText)
-                        Toast.makeText(context, "Text recognition successful", Toast.LENGTH_SHORT)
-                            .show()
+                        if (visionText.textBlocks.isEmpty()) {
+                            Toast.makeText(
+                                context,
+                                "No text found in the picture",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            tryExtractDateFromString(visionText.textBlocks)
+                        }
                     }
                     .addOnFailureListener { e ->
                         Toast.makeText(
                             context,
                             "Text recognition failed: $camComp. ${e.localizedMessage}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+            }
+        }
+
+    private var barcodeRecognitionResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+
+                val data = result.data?.extras
+                val bitmap = data?.get("data") as Bitmap
+                val camComp = CameraUtils.getRotationCompensation(requireActivity())
+                val image = InputImage.fromBitmap(bitmap, camComp)
+
+                val options = BarcodeScannerOptions.Builder()
+                    .setBarcodeFormats(
+                        Barcode.FORMAT_ALL_FORMATS
+                    )
+                    .build()
+
+                val scanner = BarcodeScanning.getClient(options)
+
+                scanner.process(image)
+                    .addOnSuccessListener { barcodes ->
+
+                        for (bar in barcodes) {
+                            Toast.makeText(
+                                context,
+                                "${bar.rawValue}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(
+                            context,
+                            "No barcode found",
                             Toast.LENGTH_SHORT
                         ).show()
                     }
@@ -80,6 +130,9 @@ class FoodCreateFragment(
         binding.foodCreateCameraButton.setOnClickListener {
             getExpirationDateFromImage()
         }
+        binding.foodCreateBarecodeButton.setOnClickListener {
+            scanBarcodeImage()
+        }
 
         return binding.root
     }
@@ -87,15 +140,48 @@ class FoodCreateFragment(
     private fun getDateFromInt(year: Int, month: Int, dayOfMonth: Int): Long {
         val calendar = Calendar.getInstance()
         calendar.set(year, month, dayOfMonth)
-
         return calendar.time.time
+    }
+
+    private fun tryExtractDateFromString(textBlocks: MutableList<Text.TextBlock>) {
+        val regex =
+            """(0?[1-9]|[12][0-9]|3[01])[- /.:](0?[1-9]|1[012])[- /.:](19|20)\d\d""".toRegex()
+
+        var matchResult: MatchResult? = null
+        for (block in textBlocks){
+            matchResult = regex.find(block.text)
+
+            if (matchResult != null){
+                setDateOnCalendar(matchResult.value)
+                return@tryExtractDateFromString
+            }
+        }
+
+        Toast.makeText(
+            context,
+            "No date detected in the picture",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun setDateOnCalendar(dateString: String) {
+        val dateResult = DateUtils.tryParseDate(dateString)
+
+        if (dateResult != null) {
+            binding.foodCreateCalendarView.date = dateResult.time
+            Toast.makeText(
+                context,
+                "Date extracted from picture: ${dateResult.time.toShortDateString()}",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 
     private fun getExpirationDateFromImage() {
 
         if (allPermissionsGranted()) {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            resultLauncher.launch(intent)
+            dateRecognitionResultLauncher.launch(intent)
         } else {
             ActivityCompat.requestPermissions(
                 requireActivity(),
@@ -105,9 +191,15 @@ class FoodCreateFragment(
         }
     }
 
+    private fun scanBarcodeImage() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        barcodeRecognitionResultLauncher.launch(intent)
+    }
+
     private fun allPermissionsGranted() = CameraUtils.CAMERA_REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             requireContext(), it
         ) == PackageManager.PERMISSION_GRANTED
     }
 }
+
---
+++ app/src/main/java/mateusz/oleksik/remindeme/utils/DateUtils.kt
@@ -0,0 +1,22 @@
+package mateusz.oleksik.remindeme.utils
+
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DateUtils {
+    companion object{
+        private val supportedFormats = listOf("dd-MM-yyyy", "dd:MM:yyyy", "dd/MM/yyyy", "dd.MM.yyyy")
+
+        fun tryParseDate(dateString: String): Date? {
+            for (format in supportedFormats){
+                try {
+                    return SimpleDateFormat(format).parse(dateString)
+                }
+                catch (ex: ParseException) {}
+            }
+
+            return null
+        }
+    }
+}
